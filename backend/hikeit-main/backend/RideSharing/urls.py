a47fa3bcd17741fad3478500519653d5c1124340
mr-Amanroy274 <101966522+mr-Amanroy274@users.noreply.github.com>
2023-04-17 13:27:25 +0545

    backend

--- backend/hikeit-main/backend/RideSharing/urls.py
+++ backend/hikeit-main/backend/RideSharing/urls.py
@@ -19,3 +19,26 @@ urlpatterns = [
 	path('delete/<str:phoneNumber>', views.deleteUser, name='delete_user'),
 
 ]
+
+
+
+
+
+
+
+
+urlpatterns = [
+	path('createuser/', views.createUser,name='Create_user'),
+	#path('', views.index, name='index'),
+	path('matchuser',matchUsers),
+	path('login', authenticateUser.as_view()),
+	path('', views.userInformation.as_view()),# --> this handles all the get post put and delete request
+	path('sendotp', sendOTP.as_view()),
+	path('verifyotp', verifyOTP),
+	#path('users/', views.getUserInfo, name='Get_all_users'),
+	path('users/<str:phoneNumber>', views.getOneUserInfo, name='Get_single_user_info'),
+	
+	path('update/<str:phoneNumber>', views.updateUser, name='Update_user_info'),
+	path('delete/<str:phoneNumber>', views.deleteUser, name='delete_user'),
+
+]
